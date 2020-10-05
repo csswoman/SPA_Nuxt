ef50b76b24a2dda756020987026daf15d45f838a
Karla Agraz <karla.agraz@gmail.com>
2020-10-05 13:53:31 -0500

    uso de componentes

---
+++ components/AlbumCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>{{ album.title }}</h3>
+    <a href="#">Ver álbum</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumCard",
+  props: {
+    album: Object,
+  },
+};
+</script>
+
+<style scoped></style>
