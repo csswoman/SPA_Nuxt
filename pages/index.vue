e18c54d69fa9b3827dcebcf237f2a8cd9b0d9617
Karla Agraz <karla.agraz@gmail.com>
2020-10-05 13:42:50 -0500

    Axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <template>
-  <h1>Hola mundo</h1>
+  <h1>{{ mensaje }}</h1>
 </template>
 
 <script>
+import axios from "axios";
+import env from "../config/env";
 export default {
   name: "IndexPage",
+
+  data() {
+    return {
+      mensaje: "",
+    };
+  },
+
+  mounted() {
+    axios.get(`${env.endpoint}/albums`).then((response) => {
+      this.mensaje = response.status;
+    });
+  },
 };
 </script>
 
